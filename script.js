3e30a2ef7b7132a596efc06ae864034576d7cf20
cridder <cjridder@gmail.com>
2022-11-05 13:53:04 -0700

    YES works

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // Assignment code here
 
-// - [ ] WHEN I click the button to generate a password
-// - [ ] THEN I am presented with a series of prompts for password criteria
+//  [ ] WHEN I click the button to generate a password
+//    [ ] THEN I am presented with a series of prompts for password criteria
+
+//  [ ] WHEN prompted for password criteria
+//    [ ] THEN I select which criteria to include in the password
 
 // Special Characters
 // "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-// main function
-function userPrompts() {
+// START FUNCTION
+function generatePassword() {
 
-// - [X] WHEN prompted for the length of the password
-// - [X] THEN I choose a length of at least 8 characters and no more than 128 characters
+//  [X] WHEN prompted for the length of the password
+//    [X] THEN I choose a length of at least 8 characters and no more than 128 characters
 // https://www.reddit.com/r/javascript/comments/196ahg/can_anyone_help_with_validation_of_prompt_within/
 var pwLengthInput; 
 do {
@@ -18,52 +21,66 @@ do {
 } while (isNaN(pwLengthInput) || pwLengthInput < 8 || pwLengthInput > 128);
 // convert input to a number
 var pwLength = parseInt(pwLengthInput);
+console.log("pwLength type is a: " + typeof pwLength);
+console.log("pwLength is : " + pwLength);
+
+
+//  [ ] WHEN asked for character types to include in the password
+//    [ ] THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// https://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip
+
+// pwLowercaseInput = prompt("\nInclude lowercase characters in password?\n \nYES or NO?\n");
+
+
+var pwLowercaseInput; 
+var pwYES = 'YES'; 
+var pwNO = 'NO'; 
+do {
+  pwLowercaseInput = prompt("\nInclude lowercase characters in password?\n \nYES or NO?\n");
+  console.log(pwLowercaseInput);
+  pwLowercaseInput = String(pwLowercaseInput);
+  console.log(pwLowercaseInput);
+  pwLowercaseInput = pwLowercaseInput.toUpperCase();
+  console.log(pwLowercaseInput);
+} while (!(pwLowercaseInput === pwYES) || (pwLowercaseInput === pwNO));
+// } while (pwLowercaseInput !== "YES" || pwLowercaseInput !== "NO");
+// } while (pwLowercaseInput !== String(pwLowercaseInput) || pwLowercaseInput !== String(pwLowercaseInput));
+// } while (isNaN(pwLowercaseInput) && pwLowercaseInput != pwLowercaseInput.toUpperCase());
+// } while (pwLowercaseInput.toLowerCase() || pwLowercaseInput != pwLowercaseInput.toUpperCase());
+// } while (!isNaN(pwLowercaseInput) && pwLowercaseInput.toLowerCase() || pwLowercaseInput != pwLowercaseInput.toUpperCase());
+// } while (pwLowercaseInput === "YES" || pwLowercaseInput === "NO" );
+
+
+// get YES or NO
+console.log("");
+console.log("");
+console.log("pwLowercaseInput type is not a number: " + !isNaN(pwLowercaseInput));
+console.log("pwLowercaseInput type is a: " + typeof pwLowercaseInput);
+console.log("pwLowercaseInput is : " + pwLowercaseInput);
+console.log("");
+console.log("");
 
 
 
 
-
-// pwLength = prompt("\n \n Length of password? \n \n 8 character minimum \n \n 128 character maximum \n \n ");
-
-// if (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
-//   prompt("Input not valid");
-// } else {
-//   prompt("Input OK");
-// }
-
-// // if pwLength is not a number, less than 8, greater than 128, ask again
-// if (!(typeof pwLength === "number")) {
-//   pwLength = prompt("\n \n You did not enter a number! \n \n 8 character minimum \n \n 128 character maximum \n \n ");
-// }
-
-
-  console.log("pwLength is a: " + typeof pwLength);
-  console.log("pwLength type is a: " + !(typeof pwLength === "number"));
-  console.log("pwLength is a: " + (pwLength < 8 || pwLength > 128));
-
-  // // if pwLength is not a number, less than 8, greater than 128, ask again
-  // if (!(typeof pwLength === "number") && (pwLength < 8 && pwLength > 128)) {
-  //   pwLength = prompt("Length of password?");
-  // }
-  // console.log(pwLength);
 }
-
-// - [ ] WHEN prompted for password criteria
-//     - [ ] THEN I select which criteria to include in the password
-// - [ ] WHEN prompted for the length of the password
-//     - [ ] THEN I choose a length of at least 8 characters and no more than 128 characters
-// - [ ] WHEN asked for character types to include in the password
-//     - [ ] THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// - [ ] WHEN I answer each prompt
-//     - [ ] THEN my input should be validated and at least one character type should be selected
-// - [ ] WHEN all prompts are answered
-//     - [ ] THEN a password is generated that matches the selected criteria
-// - [ ] WHEN the password is generated
-//     - [ ] THEN the password is either displayed in an alert or written to the page
+// END FUNCTION
 
 
-// run main function
-userPrompts();
+
+
+//  [ ] WHEN asked for character types to include in the password
+//    [ ] THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+//  [ ] WHEN I answer each prompt
+//    [ ] THEN my input should be validated and at least one character type should be selected
+
+//   [ ] WHEN all prompts are answered
+//    [ ] THEN a password is generated that matches the selected criteria
+
+//  [ ] WHEN the password is generated
+//    [ ] THEN the password is either displayed in an alert or written to the page
+
 
 
 // Provided functions below
